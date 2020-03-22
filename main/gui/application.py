eb5ad18fcaff840c69765f60ba9a86331e813212
FokaKefir <40315291+FokaKefir@users.noreply.github.com>
2020-03-22 17:19:35 +0200

    Adding the application code

---
+++ main/gui/application.py
@@ -0,0 +1,78 @@
+import tkinter as tk
+from tkinter import *
+import time
+import os
+
+def stop():
+	win.destroy()
+
+def updateStrings():
+	try:
+		newPath = os.path.relpath("..\\informations\\distance.info", currentPath)
+		fileDistance = open(newPath, "r")
+	
+		distanceLeft = fileDistance.readline()
+		distanceCenter = fileDistance.readline()
+		distanceRight = fileDistance.readline() + '\n'
+
+		strDistanceLeft.set(distanceLeft + " cm")
+		strDistanceCenter.set(distanceCenter + " cm")
+		strDistanceRight.set(distanceRight + " cm")
+	
+		fileDistance.close()
+	except:
+		pass
+	
+	try:
+		newPath = os.path.relpath("..\\informations\\object.info", currentPath)
+		fileObject = open(newPath, "r")
+		obejct = fileObject.read()
+		strObject.set("Obejct: " + obejct)
+		fileObject.close()
+	except:
+		pass
+
+def updateRunFile(strOut):
+	while True:
+		try:
+			newPath = os.path.relpath("..\\informations\\run.info", currentPath)
+			fileRun = open(newPath, "w")
+			fileRun.write(strOut)
+			fileRun.close()
+			break
+		except:
+			pass
+
+
+win = tk.Tk()
+win.title("Informations")
+
+strDistanceLeft = StringVar()
+strDistanceCenter = StringVar()
+strDistanceRight = StringVar()
+strObject = StringVar()
+
+Label(win, text="Left distance").grid(row=0, column=0)
+Label(win, text="Center distance").grid(row=0, column=1)
+Label(win, text="Right distance").grid(row=0, column=2)
+labelLeft = Label(win, text="0 cm", textvariable=strDistanceLeft).grid(row=1, column=0)
+labelCenter = Label(win, text="0 cm", textvariable=strDistanceCenter).grid(row=1, column=1)
+labelRight = Label(win, text="0 cm", textvariable=strDistanceRight).grid(row=1, column=2)
+labelObject = Label(win, text="Object: ?", textvariable=strObject).grid(row=2, column=0)
+button = tk.Button(win, text="Stop", command=lambda: stop()).grid(row=2, column=2)
+
+currentPath = os.path.dirname(__file__)
+
+print("Windows is open.")
+updateRunFile("open")
+
+while True:
+	updateStrings()
+	try:
+		win.update()
+	except :
+		print("Windows is closed.")
+		updateRunFile("close")
+		break
+		
+
